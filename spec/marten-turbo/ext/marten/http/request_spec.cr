1a4aadfb4770240595665535e85b8801f2a7d63c
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2024-03-19 15:46:25 +0100

    Extend marten request with `#turbo?` method

---
+++ spec/marten-turbo/ext/marten/http/request_spec.cr
@@ -0,0 +1,27 @@
+require "./spec_helper"
+
+require "./spec_helper"
+
+describe Marten::HTTP::Request do
+  describe "#turbo?" do
+    it "correctly returns true if the request is a turbo request" do
+      request = Marten::HTTP::Request.new(
+        method: "GET",
+        resource: "/test/xyz",
+        headers: HTTP::Headers{"Accept" => "text/vnd.turbo-stream.html"},
+      )
+
+      request.turbo?.should be_true
+    end
+
+    it "correctly returns false if the request is not a turbo request" do
+      request = Marten::HTTP::Request.new(
+        method: "GET",
+        resource: "/test/xyz",
+        headers: HTTP::Headers{"Accept" => "text/html"},
+      )
+
+      request.turbo?.should be_false
+    end
+  end
+end
---
+++ src/marten_turbo/ext/marten/http/request.cr
@@ -0,0 +1,5 @@
+class Marten::HTTP::Request
+  def turbo?
+   accepts? "text/vnd.turbo-stream.html"
+  end
+end
