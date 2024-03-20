830d38b4561043df9c968d949eaa8e7d1e273efe
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2024-03-20 15:36:50 +0100

    Add more specs for `RecordDelete`

--- spec/marten-turbo/handlers/record_delete_spec.cr
+++ spec/marten-turbo/handlers/record_delete_spec.cr
@@ -12,8 +12,8 @@ describe MartenTurbo::Handlers::RecordCreate do
           method: "GET",
           resource: "",
           headers: HTTP::Headers{
-            "Accept"       => "text/html",
-            "Host" => "example.com",
+            "Accept" => "text/html",
+            "Host"   => "example.com",
           }
         )
       )
@@ -36,8 +36,8 @@ describe MartenTurbo::Handlers::RecordCreate do
           method: "GET",
           resource: "",
           headers: HTTP::Headers{
-            "Accept"       => "text/html",
-            "Host" => "example.com",
+            "Accept" => "text/html",
+            "Host"   => "example.com",
           }
         )
       )
@@ -48,6 +48,53 @@ describe MartenTurbo::Handlers::RecordCreate do
       response.should be_a Marten::HTTP::Response::Found
       Tag.get(pk: tag_1.pk).should be_nil
       Tag.get(pk: tag_2.pk).should eq tag_2
+    end
+
+    it "deletes the record and returns the expected redirect when a turbo request was made and no template is given" do
+      tag_1 = Tag.create!(name: "Tag 1")
+      tag_2 = Tag.create!(name: "Tag 2")
+
+      params = Marten::Routing::MatchParameters{"pk" => tag_1.pk!}
+      request = Marten::HTTP::Request.new(
+        ::HTTP::Request.new(
+          method: "GET",
+          resource: "",
+          headers: HTTP::Headers{
+            "Accept" => "text/html",
+            "Host"   => "example.com",
+          }
+        )
+      )
+      handler = MartenTurbo::Handlers::RecordDeleteSpec::TestWithoutTemplateHandler.new(request, params)
+
+      response = handler.post
+
+      response.should be_a Marten::HTTP::Response::Found
+      Tag.get(pk: tag_1.pk).should be_nil
+      Tag.get(pk: tag_2.pk).should eq tag_2
+    end
+
+    it "deletes the record and returns a delete turbo stream when a turbo request was made and a template is given" do
+      tag_1 = Tag.create!(name: "Tag 1")
+      tag_2 = Tag.create!(name: "Tag 2")
+
+      params = Marten::Routing::MatchParameters{"pk" => tag_1.pk!}
+      request = Marten::HTTP::Request.new(
+        ::HTTP::Request.new(
+          method: "GET",
+          resource: "",
+          headers: HTTP::Headers{
+            "Accept" => "text/vnd.turbo-stream.html",
+            "Host"   => "example.com",
+          }
+        )
+      )
+      handler = MartenTurbo::Handlers::RecordDeleteSpec::TestHandler.new(request, params)
+
+      response = handler.post
+
+      response.should_not be_a Marten::HTTP::Response::Found
+      response.content.strip.should contain "<turbo-stream action=\"remove\" target=\"tag_#{tag_1.pk!}\"></turbo-stream>"
     end
   end
 end
